128684eb237fd44117643e5cc98c5627dc20ca20
TeLoardBruh <damnloardbruh@gmail.com>
2021-09-30 15:20:23 +0700

    prep host

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,60 +1,148 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <p class="title-text">Grid Photo Gallery</p>
+    <div class="photos-grid">
+      <div
+        v-for="item in gallery"
+        :key="item.id"
+        :class="item.cardClass"
+        :style="{ 'background-image': 'url(' + item.img + ')' }"
+      >
+        <div>
+          {{ item.id }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  data() {
+    return {
+      gallery: [
+        {
+          id: "1",
+          img:
+            "https://images.pexels.com/photos/965208/pexels-photo-965208.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+          cardClass: "card card-tall card-wide",
+        },
+        {
+          id: "2",
+          img:
+            "https://images.pexels.com/photos/8188619/pexels-photo-8188619.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+          cardClass: "card card-2 card-tall",
+        },
+        {
+          id: "3",
+          img:
+            "https://images.pexels.com/photos/1320684/pexels-photo-1320684.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+          cardClass: "card",
+        },
+        { id: "4", img: "https://images.pexels.com/photos/5214313/pexels-photo-5214313.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", cardClass: "card" },
+        { id: "5", img: "./img/view.JPG", cardClass: "card" },
+        { id: "6", img: "./img/walk.JPG", cardClass: "card" },
+        {
+          id: "7",
+          img:
+            "https://images.pexels.com/photos/163185/old-retro-antique-vintage-163185.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+          cardClass: "card card-wide",
+        },
+        {
+          id: "8",
+          img:
+            "https://images.pexels.com/photos/2187662/pexels-photo-2187662.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+          cardClass: "card",
+        },
+        {
+          id: "9",
+          img:
+            "https://images.pexels.com/photos/5745029/pexels-photo-5745029.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+          cardClass: "card",
+        },
+        { id: "10", img: "./img/aDay.jpg", cardClass: "card card-wide" },
+        {
+          id: "11",
+          img: "./img/upView.JPG",
+          cardClass: "card",
+        },
+        {
+          id: "12",
+          img: "./img/icecream.jpg",
+          cardClass: "card card-wide",
+        },
+        {
+          id: "13",
+          img: "./img/food.jpg",
+          cardClass: "card",
+        },
+        {
+          id: "14",
+          img: "./img/light.jpg",
+          cardClass: "card",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.title-text {
+  color: white;
+  font-weight: bold;
+  font-size: 40px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.photos-grid {
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+  grid-row: 240px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.card {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: #353535;
+  font-size: 15rem;
+  color: #fff;
+  box-shadow: rgba(3, 8, 20, 0.1) 0px 0.15rem 0.5rem,
+    rgba(2, 8, 20, 0.1) 0px 0.075rem 0.175rem;
+  height: 100%;
+  width: 100%;
+  border-radius: 4px;
+  transition: all 500ms;
+  overflow: hidden;
+
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  margin: 5px;
 }
-a {
-  color: #42b983;
+.card-2{
+  color: #868686;
+}
+
+.card:hover {
+  box-shadow: rgba(2, 8, 20, 0.1) 0px 0.35em 1.175em,
+    rgba(2, 8, 20, 0.08) 0px 0.175em 0.5em;
+  transform: translateY(-3px) scale(1.1);
+}
+
+/* Medium screens */
+@media screen and (min-width: 600px) {
+  .card-tall {
+    grid-row: span 2 / auto;
+  }
+
+  .card-wide {
+    grid-column: span 2 / auto;
+  }
 }
 </style>
